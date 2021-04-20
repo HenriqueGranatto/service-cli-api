f9a1c3da491f5dd99235cfa298c095d36f672672
henriquegranatto` <henrique.ramires.granatto@gmail.com`>
2021-04-20 07:46:51 -0300

    .

--- extensions/template.js
+++ extensions/template.js
@@ -50,8 +50,7 @@ const createModule = async (toolbox) =>
 
     toolbox.print.success("- Adicionando: configurações do módulo no arquivo .env")
     let env = `# Configurações da API\n`
-    env += `API_PORT=`
-    env += `\n`
+    env += `API_PORT=\n`
     toolbox.filesystem.append('.env', env)
 
     toolbox.print.success("- Adicionando: módulo no arquivo service.js")
